ca09a10832b211ffe89837f7a22fa5ec702c326d
Egor Saratov <npp_board@mail.ru>
2024-02-28 22:29:58 +0300

    add Lesson13 с массивом от 0 до 9

---
+++ Lesson13/Program.cs
@@ -0,0 +1,30 @@
+﻿// Сортировка подсчетом
+
+int[] array = {3, 2, 1, 5, 9};
+
+CountingSort(array);
+
+Console.WriteLine(string.Join(", ", array));
+
+void CountingSort(int[] inputArray)
+{
+    int[] counters = new int[10]; // массив повторений
+
+    for(int i = 0; i < inputArray.Length; i++)
+    {
+        counters[inputArray[i]]++;// Тоже самое, что ниже 2 строки
+        /*int ourNumber = inputArray[i];
+        couters[ourNumber]++;*/
+    }
+
+    int index = 0;
+    for(int i = 0; i < counters.Length; i++) // Обход всего массива повторений
+    {
+        for(int j = 0; j < counters[i]; j++) // Для вставки количества повторяющихся цифр
+        {
+            inputArray[index] = i; // Записываем в исходный массив повторения
+            index++;
+        }
+
+    }
+}
