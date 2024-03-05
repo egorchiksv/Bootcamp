4193615ec0a0267e2e528442ba2168c4f7c3b25e
Egor Saratov <npp_board@mail.ru>
2024-03-05 22:24:57 +0300

    add Lesson16

--- Lesson16/main.py
+++ Lesson16/main.py
@@ -1,6 +1,9 @@
 #OpenCV для обнаружения объектов
 import cv2
-img = cv2.imread('test.jpg') # Записываем картинку в переменную
-print(img.shape)
-img = cv2.resize(img, (500, 500))
-print(img.shape)
+img = cv2.imread('D:/GeekBrains/04_Bootcamp/Lesson16/test.jpg') # Записываем картинку в переменную
+
+#img = cv2.resize(img, (500, 500))
+
+cv2.imshow('Result', img)
+
+cv2.waitKey(0)
